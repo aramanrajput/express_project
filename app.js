cae5477f9690085c481772e3315e3fe7f424cc87
arman <armanrajput992@gmail.com>
2021-11-19 18:12:42 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express")
+let data = require("./MOCK_DATA.json")
+
+
+const app = express()
+
+function logger(req,res,next){
+    console.log(req.method)
+    next()
+}
+app.use(logger)
+
+
+
+  
+
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to home page")
+})
+
+
+app.get("/users",(req,res)=>{
+    res.send(data)
+})
+
+app.listen(2345,()=>{
+    console.log("listening to port 2345")
+})
+
+
